fde6291689503345b615da389c80f35891b08f7c
Nicolas Corbett <nicolas.corbett@student.ehb.be>
2021-05-23 12:43:58 +0200

    created: added js and html index files

---
+++ javascript/index.js
@@ -0,0 +1,17 @@
+const drivers = [
+    {age: 19, name: "Liam", speed: 20},
+    {age: 25, name: "Alex", speed: 30},
+    {age: 27, name: "Nick", speed: 400},
+    {age: 32, name: "Kiki", speed: 50},
+    {age: 17, name: "Nico", speed: 90},
+    {age: 36, name: "Jeroen", speed: 2000},
+    {age: 29, name: "Niels", speed: 70},
+    {age: 20, name: "Arno", speed: 35},
+    {age: 45, name: "Pim", speed: 25},
+    {age: 15, name: "Rob", speed: 55},
+    {age: 20, name: "Brent", speed: 60},
+    {age: 24, name: "Toto", speed: 120},
+    {age: 21, name: "Robbe<3", speed: 69420}
+   ];
+
+  
